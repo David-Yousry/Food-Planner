87d555486866a8dd1fe9e94eff12f02eb6582f6b
David_Yousry <davidyousry45@gmail.com>
2024-08-24 00:34:33 +0300

    edits

--- app/src/main/java/com/example/foodplanner/GuestProfileActivity.kt
+++ app/src/main/java/com/example/foodplanner/GuestProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.foodplanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -10,17 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 
 class GuestProfileActivity : AppCompatActivity() {
     lateinit var loginBtn: ConstraintLayout
+    lateinit var backButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_guest_profile)
         loginBtn = findViewById(R.id.logInBtn)
+        backButton = findViewById(R.id.backButton)
 
         loginBtn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
-            this.finish()
+            finish()
         }
+        backButton.setOnClickListener {
+            finish()
+        }
+
 
     }
 }
